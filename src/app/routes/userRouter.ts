05ed681c94585607e75df0ca7ce7578fdf7a6277
Roberto Luna <133290380+R0BERT10@users.noreply.github.com>
2024-08-10 01:18:45 +0000

    Create routes

---
+++ src/app/routes/userRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/:uid", );
+userRouter.delete("/:id", );
+userRouter.post("/",  );
+userRouter.put("/:id", );
+
+export default userRouter
