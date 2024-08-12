836c8df975d5b8464f88a85ef12b1e3a58bf5807
Roberto Luna <estoquefripal@gmail.com>
2024-08-12 01:23:13 -0300

    Feat: Update in new Utilities tools features.

---
+++ src/util/CreateOptionsForFetch.ts
@@ -0,0 +1,14 @@
+const post = (bodyData: any) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bodyData),
+    };
+    return options
+}
+
+export const options = {
+    POST:post
+}
--- src/util/ResultErrors.ts
+++ src/util/ResultErrors.ts
@@ -5,11 +5,13 @@ export type ResultError = {
 }
 
 export class ClientError implements ResultError {
-    static readonly enums : string[] = ["NOT_FOUND", "CONFLICT"]
+    static readonly enums : string[] = ["NOT_FOUND", "CONFLICT", "UNAUTHORIZED"]
     
     static readonly NOT_FOUND = (origin:string) => {return new ClientError("NOT_FOUND", 404, "Object not found.", origin)}
     static readonly CONFLICT = (conflictMessage:string, origin:string) => {return new ClientError("CONFLICT", 409, conflictMessage, origin)}
-
+    static readonly UNAUTHORIZED = (origin:string) => {return new ClientError("UNAUTHORIZED", 401, "Access denied", origin)}
+    
+    static readonly generic = (message:string, origin:string) => {return new ClientError("genericError", 400, message, origin)}
     // private to disallow creating other instances of this type
     private constructor(
         private key: string,
