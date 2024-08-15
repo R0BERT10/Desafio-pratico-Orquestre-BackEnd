9b8a290ad9e858eeb9469466fc2b7bb0f606abdd
Roberto Luna <estoquefripal@gmail.com>
2024-08-15 08:23:51 -0300

    Prepare deploy on vercel.

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import "dotenv/config";
+import express from "express"
+import cors from "cors"
+import { AppDataSource } from "./database/data-source"
+import routes from "./app/routes";
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+AppDataSource.initialize().then(async () => {
+    console.log("Database OK")
+})
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,6 @@
-import "reflect-metadata"
-import "dotenv/config";
-import express from "express"
-import cors from "cors"
-import { AppDataSource } from "./database/data-source"
-import routes from "./app/routes";
+import app from "./app"
 
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-
-AppDataSource.initialize().then(async () => {
-    console.log("Database OK")
-    app.listen(3000, () => {
+app.listen(3000, () => {
         console.log("Server OK")
-    })
-})
+    }
+)
